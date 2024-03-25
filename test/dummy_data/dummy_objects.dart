b56bfb491d549484284bdc71cb54a66a649ccfa1
Roby <robysetiawan2409@gmail.com>
2024-03-25 12:52:13 +0700

    feat(dummy): dummy data for response add user in unit testing
    add dummy data for mocking response json from api after add user

---
+++ test/dummy_data/dummy_objects.dart
@@ -0,0 +1,9 @@
+import 'package:multiplatform_app_crud/domain/entities/user_entity.dart';
+
+final userAdded = UserEntity(
+    name: "dustin",
+    address: "Tangerang",
+    email: "dustin@gmail.com",
+    phoneNumber: "0849098434",
+    city: "jakarta",
+    id: "20");

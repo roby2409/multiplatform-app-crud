7ad08e43b910906f1cbfbbb03802d39a9cc081bb
Roby <robysetiawan2409@gmail.com>
2024-03-26 08:43:06 +0700

    feat: spashscreen
    add splashscreen with image from lottie

---
+++ lib/presentation/pages/splash_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:multiplatform_app_crud/common/theme.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 330,
+                  width: 300,
+                  child: Lottie.asset(
+                    'assets/imagesplash.json',
+                    width: 350,
+                    height: 350,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Welcome to ",
+                    ),
+                    Text(
+                      "CPSSOFT",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 14.h,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

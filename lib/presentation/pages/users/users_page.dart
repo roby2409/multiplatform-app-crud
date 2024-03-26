f4aada7d20396bec388bcae1aca4a5319fced27f
Roby <robysetiawan2409@gmail.com>
2024-03-26 09:01:13 +0700

    feat(init): users page
    create initialize users page for showing all users

---
+++ lib/presentation/pages/users/users_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UsersPage extends StatefulWidget {
+  const UsersPage({super.key});
+
+  @override
+  State<UsersPage> createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("users page")),
+    );
+  }
+}
